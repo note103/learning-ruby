19b58643549c4433942f3db10647254527ea13a5
KADOMATSU Hiroaki <note103@gmail.com>
2019-03-24 14:23:07 +0900

    ハッシュの練習

---
+++ ゼロからわかるRuby超入門/chapter-06/sample/hash.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+hash = {"coffee" => 301, "caffe_latte" => 401}
+# p hash
+puts hash["coffee"]
+
+hash2 = {:coffee => 302, :caffe_latte => 402}
+# p hash2
+puts hash2[:coffee]
+
+hash3 = {coffee: 303, caffe_latte: 403}
+# p hash3
+puts hash3[:coffee]
